129c425b4a7d054297971f4d1fddf1423c9ae9fb
jchen <1443616037@qq.com>
2019-09-22 17:26:11 +0800

    first step

--- src/main/java/com/jchen/shiro_demon/ShiroDemonApplication.java
+++ src/main/java/com/jchen/shiro_demon/ShiroDemonApplication.java
@@ -1,9 +1,11 @@
 package com.jchen.shiro_demon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.jchen.shiro_demon.mapper"})
 public class ShiroDemonApplication {
 
 	public static void main(String[] args) {
